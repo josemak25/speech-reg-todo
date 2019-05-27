101ef535624335d602bd329986b44b74980e318b
josemak25 <amakirij@gmail.com>
2019-05-27 14:41:29 +0100

    complete project skeleton setup

---
+++ index.js
@@ -0,0 +1,8 @@
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const cat = new Animal('cat');
+console.log('it is working properly', cat);
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+// const webpack = require('webpack');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'public/main'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] }
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    publicPath: '/main/'
+  }
+};
